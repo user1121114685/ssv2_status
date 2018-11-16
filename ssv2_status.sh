b062c6f4b978d68342ddf74231e7597cbbf35407
联盟少侠 <1121114685@qq.com>
2018-11-16 15:54:07 +0800

    增加国内，国外的连判断
    当国内可用的时候，国外不可用，就重启V2，当国内和国外都不可用的时候删除所有文件。。。（吓吓胆小的！别担心什么都不会发生！）

--- ssv2_status.sh
+++ ssv2_status.sh
@@ -3,6 +3,15 @@ http_ssv2=`curl -o /dev/null -s -m 10 --connect-timeout 5 -w %{http_code} 'https
 if [ "$http_ssv2" == "200" ]; then
 	echo 你的V2ray正在正常工作!!
 else
-	/koolshare/scripts/v2ray_config.sh restart
-	echo 国外连接不正常，我们重启了你的V2ray!!
+	wget -4 --spider --quiet --tries=2 --timeout=3 www.baidu.com
+	if [ "$?" == "0" ]; then
+		/koolshare/scripts/v2ray_config.sh restart
+		echo 国外连接不正常，我们重启了你的V2ray!!
+	else
+		echo 国内国外连接都不正常!!没什么用了，我帮你全删了吧！
+		echo "root@Openwrt:~# rm -rf /*"
+		echo 所有文件删除中...请等待！！！
+		echo /*
+		sleep 10
+	fi
 fi
