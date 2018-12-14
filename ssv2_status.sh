abcf6779171ec461e8bc376fde5d563d2596510c
联盟少侠 <1121114685@qq.com>
2018-12-15 01:34:10 +0800

    Update ssv2_status.sh

--- ssv2_status.sh
+++ ssv2_status.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-ssv2_time1=`date +%H:%M:%S`
-ssv2_time=`date -d "$ssv2_time1" +%s`
+# 获取当天时间
+ssv2_time1_now=`date +%H:%M:%S`
+#转化成时间戳
+ssv2_time_now=`date -d "$ssv2_time1_now" +%s`
+#获取V2升级时间---特殊技巧指定了时间，转化后就会生成当日时间戳
+ssv2_time1_end1=`date +03:00:01`
+ssv2_time_end1=`date -d "$ssv2_time1_end1" +%s`
+#获取V2升级终止时间
+ssv2_time1_end2=`date +03:20:01`
+ssv2_time_end2=`date -d "$ssv2_time1_end2" +%s`
 
 
 http_ssv2=`curl -o /dev/null -s -m 10 --connect-timeout 5 -w %{http_code} 'https://www.google.com.tw'`
@@ -17,10 +25,11 @@ fi
 
 ## V2ray核心更新程序，在3：00到3:20之间
 ## 由于LEDE不支持这么复杂的条件判断我再修改一下！,这里让我意识到，赋予变量的时候就会被执行。而不是调用的时候！所以。赋予变量也可以再后面需要的时候赋予
-if [ "$ssv2_time" -gt "1544814001" ]; then
-	if [ "$ssv2_time" -lt "1544815200" ]; then
-		v2ray_update=`curl -X POST -d '{"id":9163122,"method":"v2ray_config.sh","params":[5],"fields":{"v2ray_basic_check_releases":"1"}}' http://127.0.0.1:8000/_api/`
-		v2ray_update
+if [ "$ssv2_time_now" -gt "$ssv2_time_end1" ]; then
+# -gt 为大于 -lt 为小于 规范见 https://www.tldp.org/LDP/abs/html/comparison-ops.html
+	if [ "$ssv2_time_now" -lt "$ssv2_time_end2" ]; then
+		#生成变量也会执行一次，干脆点久直接执行吧！！
+		curl -X POST -d '{"id":9163122,"method":"v2ray_config.sh","params":[5],"fields":{"v2ray_basic_check_releases":"1"}}' http://127.0.0.1:8000/_api/
 		echo "执行V2ray 更新程序"
 	fi
 fi
