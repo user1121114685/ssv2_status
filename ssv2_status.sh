014a5d954949296ab652227c2925d7926f3dcecb
联盟少侠 <1121114685@qq.com>
2019-04-23 08:26:03 +0800

    技术进步之后反过来修改下原来的缺陷
    原来真心很多地方不必要，祝愿我们越来越强大。

--- ssv2_status.sh
+++ ssv2_status.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # 获取当天时间
-ssv2_time1_now=`date +%H:%M:%S`
-#转化成时间戳
-ssv2_time_now=`date -d "$ssv2_time1_now" +%s`
-#获取V2升级时间---特殊技巧指定了时间，转化后就会生成当日时间戳
-ssv2_time1_end1=`date +03:00:01`
-ssv2_time_end1=`date -d "$ssv2_time1_end1" +%s`
-#获取V2升级终止时间
-ssv2_time1_end2=`date +03:20:01`
-ssv2_time_end2=`date -d "$ssv2_time1_end2" +%s`
+# ssv2_time1_now=`date +%H:%M:%S`
+# #转化成时间戳
+# ssv2_time_now=`date -d "$ssv2_time1_now" +%s`
+# #获取V2升级时间---特殊技巧指定了时间，转化后就会生成当日时间戳
+# ssv2_time1_end1=`date +03:00:01`
+# ssv2_time_end1=`date -d "$ssv2_time1_end1" +%s`
+# #获取V2升级终止时间
+# ssv2_time1_end2=`date +03:20:01`
+# ssv2_time_end2=`date -d "$ssv2_time1_end2" +%s`
 
 # v2ray_basic_enable=0
 
@@ -19,11 +19,12 @@ if [ "$http_ssv2" == "200" ]; then
 else
 	wget -4 --spider --quiet --tries=2 --timeout=3 www.baidu.com
 	if [ "$?" == "0" ]; then
-		# 获取v2ray的主开关是否打开
-		ssv2_enable=`curl -X GET  'http://127.0.0.1:8000/_api/v2ray_basic_enable'`
-		# 精简获取到的数据
-		ssv2_v2ray_enable=`echo ${ssv2_enable} | grep v2ray_basic_enable | cut -d\" -f6`
-		# 如果主开关打开就执行下面的数据
+		# # 获取v2ray的主开关是否打开
+		# ssv2_enable=`curl -X GET  'http://127.0.0.1:8000/_api/v2ray_basic_enable'`
+		# # 精简获取到的数据
+		# ssv2_v2ray_enable=`echo ${ssv2_enable} | grep v2ray_basic_enable | cut -d\" -f6`
+		# # 如果主开关打开就执行下面的数据
+		ssv2_v2ray_enable=`dbus get v2ray_basic_enable`
 		if [ "$ssv2_v2ray_enable" == "1" ];then 
 		/koolshare/scripts/v2ray_config.sh restart
 		echo 国外连接不正常，我们重启了你的V2ray!!
@@ -33,12 +34,19 @@ fi
 
 ## V2ray核心更新程序，在3：00到3:20之间
 ## 由于LEDE不支持这么复杂的条件判断我再修改一下！,这里让我意识到，赋予变量的时候就会被执行。而不是调用的时候！所以。赋予变量也可以再后面需要的时候赋予
-if [ "$ssv2_time_now" -gt "$ssv2_time_end1" ]; then
-# -gt 为大于 -lt 为小于 规范见 https://www.tldp.org/LDP/abs/html/comparison-ops.html
-	if [ "$ssv2_time_now" -lt "$ssv2_time_end2" ]; then
-		#生成变量也会执行一次，干脆点久直接执行吧！！
-		curl -X POST -d '{"id":9163122,"method":"v2ray_config.sh","params":[5],"fields":{"v2ray_basic_check_releases":"1"}}' http://127.0.0.1:8000/_api/
-		echo "执行V2ray 更新程序"
-	fi
-fi
+# if [ "$ssv2_time_now" -gt "$ssv2_time_end1" ]; then
+# # -gt 为大于 -lt 为小于 规范见 https://www.tldp.org/LDP/abs/html/comparison-ops.html
+# 	if [ "$ssv2_time_now" -lt "$ssv2_time_end2" ]; then
+# 		#生成变量也会执行一次，干脆点久直接执行吧！！
+# 		curl -X POST -d '{"id":9163122,"method":"v2ray_config.sh","params":[5],"fields":{"v2ray_basic_check_releases":"1"}}' http://127.0.0.1:8000/_api/
+# 		echo "执行V2ray 更新程序"
+# 	fi
+# fi
 
+[ ! -f  "/etc/crontabs/root" ] && touch /etc/crontabs/root
+v2ray_Core_update=`cat /etc/crontabs/root|grep v2ray_Core_update.sh`	
+if [ -z "$v2ray_Core_update" ];then
+	echo "#!/bin/sh" > /koolshare/scripts/v2ray_Core_update.sh
+	echo "curl -X POST -d '{"id":9163122,"method":"v2ray_config.sh","params":[5],"fields":{"v2ray_basic_check_releases":"1"}}' http://127.0.0.1:8000/_api/" >> /koolshare/scripts/v2ray_Core_update.sh
+	echo  "20 3 * * * /koolshare/scripts/v2ray_Core_update.sh" >> /etc/crontabs/root
+fi
